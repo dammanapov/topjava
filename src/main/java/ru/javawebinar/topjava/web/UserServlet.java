25442de10179ba430d89a6009783d36c65eb4585
Gogs <gogs@fake.local>
2016-12-06 20:16:17 +0300

    HW1

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -8,10 +8,6 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/userList.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>User list</title>
+</head>
+<body>
+<h2>User list</h2>
+</body>
+</html>
